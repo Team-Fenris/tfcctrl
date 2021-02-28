9ef5105ae6100d668a8eed599c5e75b8594c9498
Ørjan Jacobsen <orjan@orrie.org>
2021-02-28 16:55:41 +0100

    Added comments

--- tests/requests/req-post.py
+++ tests/requests/req-post.py
@@ -6,6 +6,8 @@ post_req = { 'testkey': 'testvalue'}
 # Request URL from the user input
 url = input("Insert URL: ")
 
+# Set up and make the test ready for print
 x = requests.post(url, data = post_req)
 
+# Print the callback
 print(x.text)
