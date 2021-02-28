0bdf915129d7286c069d60ab55b9c0d5cdb91471
Ørjan Jacobsen <orjan@orrie.org>
2021-02-28 16:58:38 +0100

    Added test file for GET

---
+++ tests/requests/req-get.py
@@ -0,0 +1,13 @@
+import requests
+
+# Set requests to be sent to the server
+get_req = { 'testkey': 'testvalue'}
+
+# Request URL from the user input
+url = input("Insert URL: ")
+
+# Set up and make the test ready for print
+x = requests.get(url, params=get_req)
+
+# Print the callback
+print(x.text)
