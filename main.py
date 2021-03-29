9910864892e4c7b468869c3240f652f3c044e7a1
Ørjan Jacobsen <orjan@orrie.org>
2021-03-29 15:36:20 +0200

    Refurbished and streamlined config handling

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from modules.webserver import WebServer
-from modules.config import *
-from modules.pcap import *
+from modules.config import Config
+#from modules.pcap import *
+from modules.dnsserver import *
 
 # Set hostname and port (not required)
-hostname = 'localhost'
-port = 8090
+hostname = '0.0.0.0'
+port = 80
 
 
 # Start of program
 if __name__ == "__main__":
 
     # Create config instance
-#    config = Config()
+    config = Config()
+#    print(config.params.getProperty("webserver")["default_port"])
+    print(config.params)
 
     # Run the webserver on given hostname and port
     # If not set hostname nor port, the server will be set up using default parameters defined in config.yaml
 #    webServer = WebServer(config, hostname, port)
 #    webServer = WebServer(hostname, port)
+#    webServer = WebServer()
 
+    # Start DNS server
+#    dns_server = DNSServer()
+    
     # Capture Pcaps
-    pcap = Pcap()
-    pcap.capturePcap()
+#    pcap = Pcap()
+#    pcap.capturePcap()
